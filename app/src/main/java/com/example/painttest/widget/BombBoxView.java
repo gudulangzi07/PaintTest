09ef71a10b2f4aff5aa8ede600f78b166d1f187c
zhangjingwei <zhangjingwei123>
2017-02-23 18:27:51 +0800

    测试

--- app/src/main/java/com/example/painttest/widget/BombBoxView.java
+++ app/src/main/java/com/example/painttest/widget/BombBoxView.java
@@ -10,7 +10,7 @@ import android.graphics.Path;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.view.ViewGroup;
 
 import com.example.painttest.R;
 
@@ -21,7 +21,7 @@ import com.example.painttest.R;
  * @Description:
  * @version: 1.0
  */
-public class BombBoxView extends LinearLayout {
+public class BombBoxView extends ViewGroup {
 
     private int[] widthAndHeightPixels;//点击控件的宽高
     private int[] location;//点击控件在屏幕上坐标
@@ -32,6 +32,7 @@ public class BombBoxView extends LinearLayout {
 
     private int rectF_width;
     private int rectF_height;
+    private boolean isCustom;//是否使用自定义的宽高
 
     public BombBoxView(Context context) {
         this(context, null);
@@ -49,10 +50,13 @@ public class BombBoxView extends LinearLayout {
     public BombBoxView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         setWillNotDraw(false);
+
         if (attrs != null){
             TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.BombBoxView);
             rectF_width = typedArray.getDimensionPixelSize(R.styleable.BombBoxView_rectF_width, 0);
             rectF_height = typedArray.getDimensionPixelSize(R.styleable.BombBoxView_rectF_height, 0);
+            isCustom = typedArray.getBoolean(R.styleable.BombBoxView_isCustom_width_and_height, false);
+            typedArray.recycle();
         }
 
         init();
@@ -79,58 +83,38 @@ public class BombBoxView extends LinearLayout {
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        final int count = getChildCount();
-        int childMeasureWidth = 0;
-        int childMeasureHeight = 0;
-        int layoutWidth = 0;    // 容器已经占据的宽度
-        int layoutHeight = 0;   // 容器已经占据的宽度
-        int maxChildHeight = 0; //一行中子控件最高的高度，用于决定下一行高度应该在目前基础上累加多少
-        for(int i = 0; i<count; i++){
-            View child = getChildAt(i);
-            //注意此处不能使用getWidth和getHeight，这两个方法必须在onLayout执行完，才能正确获取宽高
-            childMeasureWidth = child.getMeasuredWidth();
-            childMeasureHeight = child.getMeasuredHeight();
-            if(layoutWidth<getWidth()){
-                //如果一行没有排满，继续往右排列
-                left = layoutWidth;
-                right = left+childMeasureWidth;
-                top = layoutHeight;
-                bottom = top+childMeasureHeight;
-            } else{
-                //排满后换行
-                layoutWidth = 0;
-                layoutHeight += maxChildHeight;
-                maxChildHeight = 0;
+        for (int i = 1; i < getChildCount(); i++) {
+            View childView = getChildAt(i);
+            //这里获取上面计算过的 子View的宽、高
+            int childWidth = childView.getMeasuredWidth();
+            int childHeight = childView.getMeasuredHeight();
 
-                left = layoutWidth;
-                right = left+childMeasureWidth;
-                top = layoutHeight;
-                bottom = top+childMeasureHeight;
-            }
-
-            layoutWidth += childMeasureWidth;  //宽度累加
-            if(childMeasureHeight>maxChildHeight){
-                maxChildHeight = childMeasureHeight;
-            }
-
-            //确定子控件的位置，四个参数分别代表（左上右下）点的坐标值
-            child.layout(left, top, right, bottom);
+            childView.layout(0, 0, childWidth , childHeight);
         }
     }
 
-//    /**
-//     * 要求所有的孩子测量自己的大小，然后根据这些孩子的大小完成自己的尺寸测量
-//     */
-//    @SuppressLint("NewApi")
-//    @Override
-//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-//        // 计算出所有的childView的宽和高
-//        measureChildren(widthMeasureSpec, heightMeasureSpec);
-//        //测量并保存layout的宽高(使用getDefaultSize时，wrap_content和match_perent都是填充屏幕)
-//        //稍后会重新写这个方法，能达到wrap_content的效果
-//        setMeasuredDimension(getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec),
-//                getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec));
-//    }
+    /**
+     * 要求所有的孩子测量自己的大小，然后根据这些孩子的大小完成自己的尺寸测量
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (isCustom){
+            setMeasuredDimension(rectF_width, rectF_height);
+        }else{
+            int parentWidthSize = 0;
+            int parentHeightSize = 0;
+            for (int i = 1; i < getChildCount(); i++) {
+                View childView = getChildAt(i);
+                // 设定子view宽、高
+                childView.measure(
+                        MeasureSpec.makeMeasureSpec(0, MeasureSpec.EXACTLY),
+                        MeasureSpec.makeMeasureSpec(0, MeasureSpec.EXACTLY));
+                parentWidthSize = childView.getMeasuredWidth();
+                parentHeightSize += childView.getMeasuredHeight();
+            }
+            setMeasuredDimension(parentWidthSize, parentHeightSize);
+        }
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
